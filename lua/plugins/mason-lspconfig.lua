8b0b4efb9126cf4664ce9edd5fd689a38ecb4739
paradox8599 <paradox8599@outlook.com>
2023-07-02 07:05:19 +1000

    Ensure non LSPs are also installed

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -6,33 +6,47 @@ return {
         ensure_installed = {
             "lua_ls",
             "pyright",
-            "black",
-            "isort",
         }
     },
     config = function()
+        -- ensore non-lsps are installed (formatter/linter/...)
+        -- https://github.com/williamboman/mason-lspconfig.nvim/issues/113
+        local registry = require("mason-registry")
+        local nonLSPs = {
+            "black",
+            "isort",
+        }
+        for _, pkg_name in pairs(nonLSPs) do
+            local ok, pkg = pcall(registry.get_package, pkg_name)
+            if ok then
+                if not pkg:is_installed() then
+                    pkg:install()
+                end
+            end
+        end
+
         local lsp = require("lspconfig")
-        local capabilities = require("cmp_nvim_lsp").default_capabilities(),
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            -- Lua
+        -- Lua
 
-            lsp.lua_ls.setup {
-                capabilities = capabilities,
-                settings = {
-                    Lua = {
-                        completion = { callSnippet = "Replace" },
-                        version = "LuaJIT",
-                        diagnostics = {
-                            globals = { "vim" },
-                        },
-                        workspace = {
-                            library = vim.api.nvim_get_runtime_file("", true),
-                            checkThirdParty = false, -- Disable annoying prompt which popup every time open vim
-                        },
-                        telemetry = { enable = false }
-                    }
+        lsp.lua_ls.setup {
+            capabilities = capabilities,
+            settings = {
+                Lua = {
+                    completion = { callSnippet = "Replace" },
+                    version = "LuaJIT",
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                    workspace = {
+                        library = vim.api.nvim_get_runtime_file("", true),
+                        checkThirdParty = false, -- Disable annoying prompt which popup every time open vim
+                    },
+                    telemetry = { enable = false }
                 }
             }
+        }
 
         -- Python
         lsp.pyright.setup {
