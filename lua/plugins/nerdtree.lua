665a898c078898f049c8356bdaa6e403e6226820
paradox8599 <paradox8599@outlook.com>
2023-07-02 21:15:03 +1000

    nerdtree

---
+++ lua/plugins/nerdtree.lua
@@ -0,0 +1,8 @@
+return {
+    "preservim/nerdtree",
+    cmd = { "NERDTree" },
+    keys = {
+        { "<leader>e", ":NERDTreeToggle<cr>", desc = "Nerdtree" },
+        { "<leader>l", ":NERDTreeFind<cr>",   desc = "Locate buffer in Nerdtree" },
+    },
+}
