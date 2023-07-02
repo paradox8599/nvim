de1b0c3ed9efdc3500f3c0fea542dbe527992117
paradox8599 <paradox8599@outlook.com>
2023-07-02 21:27:06 +1000

    Nerdtree auto close

--- lua/plugins/nerdtree.lua
+++ lua/plugins/nerdtree.lua
@@ -5,11 +5,14 @@ return {
         { "<leader>e", ":NERDTreeToggle<cr>", desc = "Nerdtree" },
         { "<leader>l", ":NERDTreeFind<cr>",   desc = "Locate buffer in Nerdtree" },
     },
-    config = function ()
+    init = function ()
         vim.cmd([[
             " Show line numbers
             let NERDTreeShowLineNumbers=1
             autocmd FileType nerdtree setlocal relativenumber
+
+            " Exit Vim if NERDTree is the only window remaining in the only tab.
+            autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
         ]])
     end
 }
