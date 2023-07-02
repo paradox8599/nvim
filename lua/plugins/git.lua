30201cb4a2e806da87052317ea72cc5e6f094322
paradox8599 <paradox8599@outlook.com>
2023-07-02 17:37:07 +1000

    conflict color

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -15,6 +15,20 @@ return {
     {
         "rhysd/conflict-marker.vim",
         event = "VeryLazy",
+        config = function()
+            vim.cmd([[
+                let g:conflict_marker_highlight_group = ''
+
+                let g:conflict_marker_begin = '^<<<<<<< .*$'
+                let g:conflict_marker_end   = '^>>>>>>> .*$'
+
+                highlight ConflictMarkerBegin guibg=#2f7366
+                highlight ConflictMarkerOurs guibg=#2e5049
+                highlight ConflictMarkerTheirs guibg=#344f69
+                highlight ConflictMarkerEnd guibg=#2f628e
+                highlight ConflictMarkerCommonAncestorsHunk guibg=#754a81
+            ]])
+        end
     },
     {
         "lewis6991/gitsigns.nvim",
