a1286c270078aded073c6ec4ac0e09b1abf26519
paradox8599 <paradox8599@outlook.com>
2023-07-02 21:19:03 +1000

    Nerdtree line numbers

--- lua/plugins/nerdtree.lua
+++ lua/plugins/nerdtree.lua
@@ -5,4 +5,11 @@ return {
         { "<leader>e", ":NERDTreeToggle<cr>", desc = "Nerdtree" },
         { "<leader>l", ":NERDTreeFind<cr>",   desc = "Locate buffer in Nerdtree" },
     },
+    config = function ()
+        vim.cmd([[
+            " Show line numbers
+            let NERDTreeShowLineNumbers=1
+            autocmd FileType nerdtree setlocal relativenumber
+        ]])
+    end
 }
