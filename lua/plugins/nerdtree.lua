8e3f42b10528a7c6f59f1a864241eb5dd569d39d
paradox8599 <paradox8599@outlook.com>
2023-07-02 22:11:13 +1000

    Added nerd tree file icon (vim-devicons)

--- lua/plugins/nerdtree.lua
+++ lua/plugins/nerdtree.lua
@@ -1,19 +1,25 @@
 return {
-    "preservim/nerdtree",
-    cmd = { "NERDTree" },
-    keys = {
-        { "<leader>e", ":NERDTreeFocus<cr>", desc = "NerdTree Focus" },
-        { "<leader>t", ":NERDTreeToggle<cr>",   desc = "NerdTree Toggle" },
-        { "<leader>l", ":NERDTreeFind<cr>",   desc = "NerdTree Locate File" },
-    },
-    init = function ()
-        vim.cmd([[
+    {
+        "preservim/nerdtree",
+        dependencies = { "ryanoasis/vim-devicons" },
+        cmd = { "NERDTree" },
+        keys = {
+            { "<leader>t", ":NERDTreeFocus<cr>",  desc = "NerdTree Focus" },
+            { "<leader>e", ":NERDTreeToggle<cr>", desc = "NerdTree Toggle" },
+            { "<leader>l", ":NERDTreeFind<cr>",   desc = "NerdTree Locate File" },
+        },
+        config = function()
+            vim.cmd([[
             " Show line numbers
             let NERDTreeShowLineNumbers=1
             autocmd FileType nerdtree setlocal relativenumber
 
             " Exit Vim if NERDTree is the only window remaining in the only tab.
             autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
+
+            " https://github.com/ryanoasis/vim-devicons/issues/215
+            call webdevicons#refresh()
         ]])
-    end
+        end
+    },
 }
