a12cc569d36779aaee2c287a15056018cbad6511
paradox8599 <paradox8599@outlook.com>
2023-07-04 18:23:53 +1000

    Firenvim never auto take over; Updates

--- lua/plugins/firenvim.lua
+++ lua/plugins/firenvim.lua
@@ -1,18 +1,23 @@
-return
-{
+return {
     'glacambre/firenvim',
+    -- enabled = false,
     -- Lazy load firenvim
     -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
     cond = not not vim.g.started_by_firenvim,
     build = function()
-        require("lazy").load({ plugins = "firenvim", wait = true })
-        vim.fn["firenvim#install"](0)
-        vim.g.firenvim_config = {
-            globalSettings = { takeover = "never" },
-        }
-        vim.api.nvim_create_autocmd({ 'BufEnter' }, {
-            pattern = "github.com_*.txt",
-            cmd = "set filetype=markdown"
+        require("lazy").load({
+            plugins = "firenvim",
+            wait = true
         })
+        vim.fn["firenvim#install"](0)
+    end,
+    config = function ()
+        vim.g.firenvim_config = {
+            localSettings = {
+                [".*"] = {
+                    takeover = 'never'
+                }
+            }
+        }
     end
 }
