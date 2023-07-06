3578bd04b8c01e1ba5f0aea025c29458505e0e62
paradox8599 <paradox8599@outlook.com>
2023-07-06 21:20:08 +1000

    Switch to nvim-lua/kickstart.nvim

--- lua/utils.lua
+++
@@ -1,9 +0,0 @@
--- highlight after copy
-vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-    pattern = { "*" },
-    callback = function()
-        vim.highlight.on_yank({
-            timeout = 300,
-        })
-    end,
-})
