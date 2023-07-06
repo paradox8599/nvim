e9eafadff55f1f712e1fa0e5572cfd3933bfcb9e
paradox8599 <paradox8599@outlook.com>
2023-07-07 00:57:54 +1000

    Keymap: <C-h/j/k/l

---
+++ lua/custom/plugins/keymaps.lua
@@ -0,0 +1,10 @@
+local nmap = function(keys, func, desc)
+  vim.keymap.set('n', keys, func, { desc = desc })
+end
+
+nmap('<C-j>', '<C-w>j', 'Window down')
+nmap('<C-k>', '<C-w>k', 'Window up')
+nmap('<C-h>', '<C-w>h', 'Window left')
+nmap('<C-l>', '<C-w>l', 'Window right')
+
+return {}
