83ec98c57c9c044818b00da831383510766913dd
paradox8599 <paradox8599@outlook.com>
2023-07-06 23:24:39 +1000

    Changed cmp keys

--- init.lua
+++ init.lua
@@ -486,7 +486,8 @@ cmp.setup {
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete {},
+    ['<C-e>'] = cmp.mapping.complete {},
+    ['<C-q>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
