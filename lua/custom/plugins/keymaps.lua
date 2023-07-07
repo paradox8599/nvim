62bce48f46d3d2392b40d99bbb2525a50d79d042
paradox8599 <paradox8599@outlook.com>
2023-07-07 10:04:28 +1000

    Keymap func add opts

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -1,5 +1,7 @@
-local nmap = function(keys, func, desc)
-  vim.keymap.set('n', keys, func, { desc = desc })
+local nmap = function(keys, func, desc, opts)
+  if not opts then opts = {} end
+  opts['desc'] = desc
+  vim.keymap.set('n', keys, func, opts)
 end
 
 nmap('<C-j>', '<C-w>j', 'Window down')
@@ -7,7 +9,8 @@ nmap('<C-k>', '<C-w>k', 'Window up')
 nmap('<C-h>', '<C-w>h', 'Window left')
 nmap('<C-l>', '<C-w>l', 'Window right')
 
-nmap('<Esc>', ':noh<CR>')
-nmap('<Leader>e', ':Neotree toggle<CR>', '[E]xplorer')
+nmap('<Esc>', ':noh<CR>', 'Remove highlight', { silent = true })
+nmap('<Leader>e', ':Neotree toggle<CR>', '[E]xplorer', { silent = true })
+nmap('<A-F>', ':Format<CR>', 'Format', { silent = true })
 
 return {}
