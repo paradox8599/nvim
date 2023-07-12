60bf72d9ef048d33507e7a12ddfef51a6c49ba7b
paradox8599 <paradox8599@outlook.com>
2023-07-13 00:05:52 +1000

    Moved auto indent to separated file

---
+++ lua/plugins/qol/indent-blankline.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    event = 'VeryLazy',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help indent_blankline.txt`
+    opts = {
+      char = '┊',
+      show_trailing_blankline_indent = false,
+    },
+  },
+}
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -21,16 +21,4 @@ return {
       },
     },
   },
-  {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    event = 'VeryLazy',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
-  },
-
 }
