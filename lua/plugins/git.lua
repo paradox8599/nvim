2fa76e120416373b93191c4824b1a64de81f4004
paradox8599 <paradox8599@outlook.com>
2023-12-04 02:08:38 +1100

    conflict highlights

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -116,5 +116,15 @@ return {
   -- Conflict marker
   {
     'rhysd/conflict-marker.vim',
+    config = function()
+      vim.cmd('let g:conflict_marker_highlight_group = ""')
+      -- Include text after begin and end markers
+      vim.cmd('highlight ConflictMarkerBegin guibg=#2f7366')
+      vim.cmd('highlight ConflictMarkerOurs guibg=#2e5049')
+      vim.cmd('highlight ConflictMarkerTheirs guibg=#344f69')
+      vim.cmd('highlight ConflictMarkerEnd guibg=#2f628e')
+      vim.cmd('highlight ConflictMarkerSeparator guibg=#000000')
+      vim.cmd('highlight ConflictMarkerCommonAncestorsHunk guibg=#754a81')
+    end
   }
 }
