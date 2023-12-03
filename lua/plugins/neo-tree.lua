90d09abe3723fed91593f6e31a5d7faf837ed240
paradox8599 <paradox8599@outlook.com>
2023-12-03 19:02:49 +1100

    neotree v3; image preview

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -3,11 +3,12 @@ vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 return {
   {
     'nvim-neo-tree/neo-tree.nvim',
-    version = '*',
+    branch = "v3.x",
     dependencies = {
       'nvim-lua/plenary.nvim',
       'nvim-tree/nvim-web-devicons',
       'MunifTanjim/nui.nvim',
+      '3rd/image.nvim'
     },
     cmd = { 'Neotree', 'Neotree toggle' },
   },
