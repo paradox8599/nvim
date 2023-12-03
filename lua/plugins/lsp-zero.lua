1c9410e3029f48fcd6dafc2467ff7ea920ea1483
paradox8599 <paradox8599@outlook.com>
2023-12-03 18:57:46 +1100

    add lsp lua configs

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -122,11 +122,11 @@ return {
         },
         handlers = {
           lsp_zero.default_setup,
-          -- lua_ls = function()
-          --   -- (Optional) Configure lua language server for neovim
-          --   local lua_opts = lsp_zero.nvim_lua_ls()
-          --   require('lspconfig').lua_ls.setup(lua_opts)
-          -- end,
+          lua_ls = function()
+            -- (Optional) Configure lua language server for neovim
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            require('lspconfig').lua_ls.setup(lua_opts)
+          end,
         },
       })
     end,
