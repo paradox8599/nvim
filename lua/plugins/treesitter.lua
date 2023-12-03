ba770011fac1db8b3f2794da75dc7f78fa90beac
paradox8599 <paradox8599@outlook.com>
2023-12-03 23:09:13 +1100

    Removed unused lang from treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -11,9 +11,6 @@ return {
       require('nvim-treesitter.configs').setup({
         -- Add languages to be installed here that you want installed for treesitter
         ensure_installed = {
-          'c',
-          'cpp',
-          'go',
           'lua',
           'python',
           'rust',
