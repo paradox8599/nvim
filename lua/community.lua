95cb6925220454e70f64d3035085ee34abd97e3a
paradox8599 <paradox8599@outlook.com>
2024-07-11 00:18:32 +1000

    updated community plugins setup

--- lua/community.lua
+++ lua/community.lua
@@ -7,16 +7,15 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
 
-  { import = "astrocommunity/bars-and-lines/smartcolumn-nvim" },
+  { import = "astrocommunity/bars-and-lines/smartcolumn-nvim" }, -- line max characters indicator
 
   { import = "astrocommunity/code-runner/overseer-nvim" }, -- task runner
   { import = "astrocommunity/color/ccc-nvim" }, -- color picker
   { import = "astrocommunity/color/mini-hipatterns" }, -- highlight patterns（comments, colors
-  -- { import = "astrocommunity/color/tint-nvim" }, -- dim inactive window
-  { import = "astrocommunity/color/transparent-nvim" },
+  { import = "astrocommunity/color/tint-nvim" }, -- dim inactive window
+  -- { import = "astrocommunity/color/transparent-nvim" },
 
-  { import = "astrocommunity/completion/cmp-cmdline" },
-  -- { import = "astrocommunity/completion/codeium-vim" },
+  { import = "astrocommunity/completion/cmp-cmdline" }, -- command auto completion
 
   -- persistent-breakpoints-nvim loaded in dap.lua
   -- { import = "astrocommunity/debugging/persistent-breakpoints-nvim" },
@@ -24,19 +23,18 @@ return {
   -- { import = "astrocommunity/debugging/nvim-dap-repl-highlights" },
   -- { import = "astrocommunity/debugging/telescope-dap-nvim" },
 
-  -- { import = "astrocommunity/diagnostics/lsp_lines-nvim" }, -- display virtual diagnostic lines
+  -- { import = "astrocommunity/diagnostics/lsp_lines-nvim" }, -- display virtual diagnostic lines (under current line)
   { import = "astrocommunity/diagnostics/trouble-nvim" }, -- diagnostic UI
 
   -- { import = "astrocommunity/editing-support/nvim-regexplainer" }, -- explain regex
   { import = "astrocommunity/editing-support/refactoring-nvim" }, -- refactoring, https://github.com/ThePrimeagen/refactoring.nvim
-  { import = "astrocommunity/editing-support/ultimate-autopair-nvim" }, -- autopair
+  -- { import = "astrocommunity/editing-support/ultimate-autopair-nvim" }, -- autopair
   -- keymaps: https://github.com/AstroNvim/astrocommunity/blob/main/lua/astrocommunity/editing-support/yanky-nvim/init.lua
   -- { import = "astrocommunity/editing-support/yanky-nvim" }, -- more yank operations
-  -- { import = "astrocommunity/editing-support/vim-move" }, -- Alt + hjkl https://github.com/matze/vim-move
 
   -- { import = "astrocommunity/lsp/garbage-day-nvim" },
   { import = "astrocommunity/lsp/inc-rename-nvim" },
-  { import = "astrocommunity/lsp/lsp-signature-nvim" }, -- function parameters hint
+  { import = "astrocommunity/lsp/lsp-signature-nvim" }, -- floating function parameters hint
   { import = "astrocommunity/lsp/nvim-lsp-file-operations" },
 
   { import = "astrocommunity/markdown-and-latex/peek-nvim" }, -- markdown preview window, requires [ deno ]
@@ -65,20 +63,14 @@ return {
 
   { import = "astrocommunity/programming-language-support/csv-vim" },
   { import = "astrocommunity/programming-language-support/nvim-jqx" }, -- json, requires [ jq ]
-  -- { import = "astrocommunity/programming-language-support/rest-nvim" }, -- requires [ curl ], optional: [ jq, tidy ]
 
   { import = "astrocommunity/scrolling/satellite-nvim" }, -- scrollbar, requires neovim 0.10+
   { import = "astrocommunity/scrolling/vim-smoothie", cond = function() return not vim.g.neovide end },
 
   { import = "astrocommunity/search/nvim-hlslens" }, -- seach reuslt index
-  -- { import = "astrocommunity/search/sad-nvim" }, -- requires [ sad, fzf, fd-find, delta (pager tool) ]
-
-  -- { import = "astrocommunity/syntax/hlargs-nvim" }, -- function arguments highlight
 
   -- { import = "astrocommunity/terminal-integration/vim-tpipeline" }, -- tmux
   -- { import = "astrocommunity/terminal-integration/flatten-nvim" }, -- flatten file buffer opened in terminal
 
   -- { import = "astrocommunity/test/neotest" },
-
-  -- { import = "astrocommunity/workflow/hardtime-nvim" }, -- https://github.com/m4xshen/hardtime.nvim
 }
