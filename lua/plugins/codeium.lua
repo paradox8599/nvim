170e6704f89c7ba29cb90e5b6677fdaf6ba9bf0e
paradox8599 <paradox8599@outlook.com>
2024-07-13 16:34:59 +1000

    added codeium

---
+++ lua/plugins/codeium.lua
@@ -0,0 +1,45 @@
+return {
+  "Exafunction/codeium.vim",
+  cmd = {
+    "Codeium",
+    "CodeiumEnable",
+    "CodeiumDisable",
+    "CodeiumToggle",
+    "CodeiumAuto",
+    "CodeiumManual",
+  },
+  event = "BufEnter",
+  dependencies = {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        n = {
+          ["<Leader>;"] = {
+            "<Cmd>CodeiumToggle<CR>",
+            noremap = true,
+            desc = "Toggle Codeium active",
+          },
+        },
+        i = {
+          ["<C-g>"] = {
+            function() return vim.fn["codeium#Accept"]() end,
+            expr = true,
+          },
+          ["<A-'>"] = {
+            function() return vim.fn["codeium#CycleCompletions"](1) end,
+            expr = true,
+          },
+          ["<A-,>"] = {
+            function() return vim.fn["codeium#CycleCompletions"](-1) end,
+            expr = true,
+          },
+          ["<C-x>"] = {
+            function() return vim.fn["codeium#Clear"]() end,
+            expr = true,
+          },
+        },
+      },
+    },
+  },
+}
