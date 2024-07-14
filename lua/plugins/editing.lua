11f7d0d58da33cfe4d779534124673350953251e
paradox8599 <paradox8599@outlook.com>
2024-07-14 20:49:09 +1000

    added surround-nvim

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -46,6 +46,14 @@ return {
     },
   },
 
+  -- nvim-surround
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    opts = {},
+  },
+
   -- speeddating: <C-a/x> for date
   { "tpope/vim-speeddating", event = "VeryLazy", config = function() end },
 
