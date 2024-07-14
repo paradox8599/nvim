560fdac5173fa6dabf0a0e6f200bf698273b4c99
paradox8599 <paradox8599@outlook.com>
2024-07-14 21:34:30 +1000

    explicitly set neo-tree to enable git status

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -39,21 +39,24 @@ return {
 
   {
     "nvim-neo-tree/neo-tree.nvim",
-    opts = function(_, opts)
-      opts.filesystem.filtered_items = {
-        hide_dotfiles = true,
-        hide_by_name = {},
-        hide_by_pattern = {},
-        always_show = {
-          ".env",
-          ".env.local",
-          ".env.prod",
-          ".env.dev",
-          ".env.test",
+    opts = {
+      enable_git_status = true,
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = true,
+          hide_by_name = {},
+          hide_by_pattern = {},
+          always_show = {
+            ".env",
+            ".env.local",
+            ".env.prod",
+            ".env.dev",
+            ".env.test",
+          },
+          never_show = {},
+          never_show_by_pattern = {},
         },
-        never_show = {},
-        never_show_by_pattern = {},
-      }
-    end,
+      },
+    },
   },
 }
