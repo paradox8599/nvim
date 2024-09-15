7833d827e268ef8863a3fc9a14bae42dd9d4eb94
paradox8599 <paradox8599@outlook.com>
2024-09-15 12:40:04 +1000

    made rainbow-delimiters lazy

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -54,14 +54,13 @@ return {
   {
     "HiPhish/rainbow-delimiters.nvim",
     dependencies = "nvim-treesitter/nvim-treesitter",
-    lazy = false,
     event = "User AstroFile",
     main = "rainbow-delimiters.setup",
     config = function()
       -- This module contains a number of default definitions
       local rainbow_delimiters = require "rainbow-delimiters"
 
-      ---@type rainbow_delimiters.config
+      -- @type rainbow_delimiters.config
       vim.g.rainbow_delimiters = {
         strategy = {
           [""] = rainbow_delimiters.strategy["global"],
