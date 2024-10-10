bf67c2ad4abe5fe660f4046005a3e0873f1452d8
paradox8599 <paradox8599@outlook.com>
2024-10-11 01:21:37 +1100

    improved vscode renaming scripts for multi platforms

--- lua/polish.lua
+++ lua/polish.lua
@@ -2,10 +2,18 @@
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
+local is_win = require("utils").is_win
+
 -- set font for neovide
 if vim.g.neovide then vim.o.guifont = "JetBrainsMono Nerd Font:h12" end
 
 local dap = require "dap"
 dap.adapters.lldb = dap.adapters.codelldb
 
-os.execute "sh scripts/alpha_replace.sh"
+local cfg_path = vim.fn.stdpath "config"
+
+if is_win then
+  os.execute(tostring("sh " .. cfg_path .. "/scripts/win/alpha_replace.sh"))
+else
+  os.execute(tostring("sh " .. cfg_path .. "/scripts/alpha_replace.sh"))
+end
--- scripts/alpha_replace.sh
+++ scripts/alpha_replace.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed -i "s/AstroNvim loaded/VSCode loaded/g" "$HOME/AppData/Local/nvim-data/lazy/AstroNvim/lua/astronvim/plugins/configs/alpha.lua"
-sleep 3 && sed -i "s/VSCode/AstroNvim/g" "$HOME/AppData/Local/nvim-data/lazy/AstroNvim/lua/astronvim/plugins/configs/alpha.lua" &
+sed -i '' "s/AstroNvim loaded/VSCode loaded/g" "$HOME/.local/share/nvim/lazy/AstroNvim/lua/astronvim/plugins/configs/alpha.lua"
+sleep 2 && sed -i '' "s/VSCode/AstroNvim/g" "$HOME/.local/share/nvim/lazy/AstroNvim/lua/astronvim/plugins/configs/alpha.lua" &
