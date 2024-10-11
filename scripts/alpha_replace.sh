ab35c214bf37893ca223ee67849716cc3e12a65e
paradox8599 <paradox8599@outlook.com>
2024-10-11 11:36:50 +1100

    reduced sleep time before restoring alpha footer

--- scripts/alpha_replace.sh
+++ scripts/alpha_replace.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sed -i '' "s/AstroNvim loaded/VSCode loaded/g" "$HOME/.local/share/nvim/lazy/AstroNvim/lua/astronvim/plugins/configs/alpha.lua"
-sleep 2 && sed -i '' "s/VSCode/AstroNvim/g" "$HOME/.local/share/nvim/lazy/AstroNvim/lua/astronvim/plugins/configs/alpha.lua" &
+sleep 1 && sed -i '' "s/VSCode/AstroNvim/g" "$HOME/.local/share/nvim/lazy/AstroNvim/lua/astronvim/plugins/configs/alpha.lua" &
