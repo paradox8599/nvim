d70b32befdac5fb95810024f934e0be771778099
paradox8599 <paradox8599@outlook.com>
2024-10-16 13:55:03 +0000

    fix: os detection and os specific scripts

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,4 +1,8 @@
-local is_win = vim.loop.os_uname().sysname == "Windows_NT"
+local is_win = jit.os == "Windows"
+local is_mac = jit.os == "OSX"
+local is_linux = jit.os == "Linux"
 return {
   is_win = is_win,
+  is_mac = is_mac,
+  is_linux = is_linux
 }
