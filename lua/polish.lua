e2f5d45aeec29bd2e45dc43c061b5c6dc32d6a86
paradox8599 <paradox8599@outlook.com>
2024-10-26 17:08:42 +1100

    use utils

--- lua/polish.lua
+++ lua/polish.lua
@@ -2,7 +2,7 @@
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
-local utils = require"utils"
+local utils = require "utils"
 
 -- set font for neovide
 if vim.g.neovide then vim.o.guifont = "JetBrainsMono Nerd Font:h12" end
@@ -12,14 +12,10 @@ dap.adapters.lldb = dap.adapters.codelldb
 
 local cfg_path = vim.fn.stdpath "config"
 
-if utils.is_win then
-  os.execute(tostring("sh " .. cfg_path .. "/scripts/win/alpha_replace.sh"))
-end
+if utils.is_win then os.execute(tostring("sh " .. cfg_path .. "/scripts/win/alpha_replace.sh")) end
 
-if utils.is_mac then
-  os.execute(tostring("sh " .. cfg_path .. "/scripts/darwin/alpha_replace.sh"))
-end
+if utils.is_mac then os.execute(tostring("sh " .. cfg_path .. "/scripts/darwin/alpha_replace.sh")) end
 
-if utils.is_linux then
-  os.execute(tostring("sh " .. cfg_path .. "/scripts/alpha_replace.sh"))
-end
+if utils.is_linux then os.execute(tostring("sh " .. cfg_path .. "/scripts/alpha_replace.sh")) end
+
+require("lspconfig").taplo.setup {}
