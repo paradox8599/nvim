df616f995355443849cbc0cb441f9aa9a8d2f038
paradox8599 <paradox8599@outlook.com>
2024-10-28 20:13:48 +1100

    formatting

--- lua/polish.lua
+++ lua/polish.lua
@@ -2,20 +2,17 @@
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
-local utils = require "utils"
-
 -- set font for neovide
 if vim.g.neovide then vim.o.guifont = "JetBrainsMono Nerd Font:h12" end
 
-local dap = require "dap"
-dap.adapters.lldb = dap.adapters.codelldb
-
+-- replace alpha footer 'AstroNvim' with 'VSCode'
+local utils = require "utils"
 local cfg_path = vim.fn.stdpath "config"
-
 if utils.is_win then os.execute(tostring("sh " .. cfg_path .. "/scripts/win/alpha_replace.sh")) end
-
 if utils.is_mac then os.execute(tostring("sh " .. cfg_path .. "/scripts/darwin/alpha_replace.sh")) end
-
 if utils.is_linux then os.execute(tostring("sh " .. cfg_path .. "/scripts/alpha_replace.sh")) end
 
-require("lspconfig").taplo.setup {}
+-- renaming rust dap
+require("dap").adapters.lldb = require("dap").adapters.codelldb
+
+-- require("lspconfig").taplo.setup {}
