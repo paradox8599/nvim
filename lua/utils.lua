535a1ab22311a4ce3419105b22441184cbace9b3
paradox8599 <paradox8599@outlook.com>
2024-11-08 22:47:32 +1100

    foramtting

--- lua/utils.lua
+++ lua/utils.lua
@@ -2,6 +2,7 @@
 local is_win = vim.loop.os_uname().sysname == "Windows_NT"
 local is_mac = vim.loop.os_uname().sysname == "Darwin"
 local is_linux = vim.loop.os_uname().sysname == "Linux"
+
 return {
   is_win = is_win,
   is_mac = is_mac,
