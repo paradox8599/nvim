0f3f8d8ad4252d741f90ca1e29fb31837672f534
paradox8599 <paradox8599@outlook.com>
2024-11-08 22:46:49 +1100

    os detection

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,8 +1,9 @@
-local is_win = jit.os == "Windows"
-local is_mac = jit.os == "OSX"
-local is_linux = jit.os == "Linux"
+-- jit.os == "Windows" | "OSX"
+local is_win = vim.loop.os_uname().sysname == "Windows_NT"
+local is_mac = vim.loop.os_uname().sysname == "Darwin"
+local is_linux = vim.loop.os_uname().sysname == "Linux"
 return {
   is_win = is_win,
   is_mac = is_mac,
-  is_linux = is_linux
+  is_linux = is_linux,
 }
