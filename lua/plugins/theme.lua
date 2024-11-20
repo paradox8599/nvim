857ad515093337e5d477401a83ee985237ba2176
paradox8599 <paradox8599@outlook.com>
2024-11-20 23:58:05 +1100

    updated

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -45,41 +45,41 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     enabled = false,
-    main = "ibl",
-    event = "VeryLazy",
-
-    config = function()
-      local highlight = {
-        -- "iblRed",
-        "iblYellow",
-        "iblBlue",
-        "iblOrange",
-        -- "iblGreen",
-        "iblViolet",
-        "iblCyan",
-      }
-
-      local hooks = require "ibl.hooks"
-      -- create the highlight groups in the highlight setup hook,
-      --  so they are reset
-      -- every time the colorscheme changes
-      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        -- vim.api.nvim_set_hl(0, "iblRed", { fg = "#E06C75" })
-        vim.api.nvim_set_hl(0, "iblYellow", { fg = "#E5C07B" })
-        vim.api.nvim_set_hl(0, "iblBlue", { fg = "#61AFEF" })
-        vim.api.nvim_set_hl(0, "iblOrange", { fg = "#D19A66" })
-        -- vim.api.nvim_set_hl(0, "iblGreen", {fg= "#98C379" })
-        vim.api.nvim_set_hl(0, "iblViolet", { fg = "#C678DD" })
-        vim.api.nvim_set_hl(0, "iblCyan", { fg = "#56B6C2" })
-      end)
-
-      require("ibl").setup {
-        indent = {
-          highlight = highlight,
-          char = "┊",
-        },
-      }
-    end,
+    -- main = "ibl",
+    -- event = "VeryLazy",
+    --
+    -- config = function()
+    --   local highlight = {
+    --     -- "iblRed",
+    --     "iblYellow",
+    --     "iblBlue",
+    --     "iblOrange",
+    --     -- "iblGreen",
+    --     "iblViolet",
+    --     "iblCyan",
+    --   }
+    --
+    --   local hooks = require "ibl.hooks"
+    --   -- create the highlight groups in the highlight setup hook,
+    --   --  so they are reset
+    --   -- every time the colorscheme changes
+    --   hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    --     -- vim.api.nvim_set_hl(0, "iblRed", { fg = "#E06C75" })
+    --     vim.api.nvim_set_hl(0, "iblYellow", { fg = "#E5C07B" })
+    --     vim.api.nvim_set_hl(0, "iblBlue", { fg = "#61AFEF" })
+    --     vim.api.nvim_set_hl(0, "iblOrange", { fg = "#D19A66" })
+    --     -- vim.api.nvim_set_hl(0, "iblGreen", {fg= "#98C379" })
+    --     vim.api.nvim_set_hl(0, "iblViolet", { fg = "#C678DD" })
+    --     vim.api.nvim_set_hl(0, "iblCyan", { fg = "#56B6C2" })
+    --   end)
+    --
+    --   require("ibl").setup {
+    --     indent = {
+    --       highlight = highlight,
+    --       char = "┊",
+    --     },
+    --   }
+    -- end,
   },
 
   -- {
