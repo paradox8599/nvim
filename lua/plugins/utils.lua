4f23fa7c122922d4668e8024b21ccae466f11927
paradox8599 <paradox8599@outlook.com>
2024-11-20 21:25:35 +1100

    added render-markdown

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -56,6 +56,20 @@ return {
     opts = {},
   },
 
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+    event = "User AstroFile",
+    keys = {
+      { "<leader>um", function() require("render-markdown").toggle() end, desc = "Toggle Render Markdown" },
+    },
+  },
+
   -- harpoon
   -- {
   --   "ThePrimeagen/harpoon",
