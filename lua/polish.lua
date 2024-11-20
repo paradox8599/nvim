3dd7bd10a7e72f30faf9c0aeefa1c0b1004b9c17
paradox8599 <paradox8599@outlook.com>
2024-11-20 23:49:55 +1100

    rm script for alpha

--- lua/polish.lua
+++ lua/polish.lua
@@ -6,11 +6,11 @@
 if vim.g.neovide then vim.o.guifont = "JetBrainsMono Nerd Font:h12" end
 
 -- replace alpha footer 'AstroNvim' with 'VSCode'
-local utils = require "utils"
-local cfg_path = vim.fn.stdpath "config"
-if utils.is_win then os.execute(tostring("sh " .. cfg_path .. "/scripts/win/alpha_replace.sh")) end
-if utils.is_mac then os.execute(tostring("sh " .. cfg_path .. "/scripts/darwin/alpha_replace.sh")) end
-if utils.is_linux then os.execute(tostring("sh " .. cfg_path .. "/scripts/alpha_replace.sh")) end
+-- local utils = require "utils"
+-- local cfg_path = vim.fn.stdpath "config"
+-- if utils.is_win then os.execute(tostring("sh " .. cfg_path .. "/scripts/win/alpha_replace.sh")) end
+-- if utils.is_mac then os.execute(tostring("sh " .. cfg_path .. "/scripts/darwin/alpha_replace.sh")) end
+-- if utils.is_linux then os.execute(tostring("sh " .. cfg_path .. "/scripts/alpha_replace.sh")) end
 
 -- renaming rust dap
 require("dap").adapters.lldb = require("dap").adapters.codelldb
