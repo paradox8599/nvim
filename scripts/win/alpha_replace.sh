c354803db0cdbfd7bcaab66a4055747c1ee81a6e
paradox8599 <paradox8599@outlook.com>
2024-11-25 20:11:48 +1100

    rm unused scripts

--- scripts/win/alpha_replace.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sed -i "s/AstroNvim loaded/VSCode loaded/g" "$HOME/AppData/Local/nvim-data/lazy/AstroNvim/lua/astronvim/plugins/configs/alpha.lua"
-sleep 1 && sed -i "s/VSCode/AstroNvim/g" "$HOME/AppData/Local/nvim-data/lazy/AstroNvim/lua/astronvim/plugins/configs/alpha.lua" &
