c7ce8dff77ab50e5e16a2096c62561328b0aba60
paradox8599 <paradox8599@outlook.com>
2024-12-09 10:20:01 +1100

    disable im-select for now

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -166,7 +166,7 @@ return {
 
   {
     "keaising/im-select.nvim",
-    enabled = not utils.is_linux,
+    enabled = false and not utils.is_linux,
     event = "VeryLazy",
     opts = {
       default_im_select = utils.is_win and "3081" or utils.is_mac and "com.apple.keylayout.Australian" or "us",
