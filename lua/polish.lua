f6c7ca1d684b69ae8740b387ba86738221c7765d
paradox8599 <paradox8599@outlook.com>
2024-12-22 04:45:40 +1100

    updated comments

--- lua/polish.lua
+++ lua/polish.lua
@@ -5,14 +5,7 @@
 -- set font for neovide
 if vim.g.neovide then vim.o.guifont = "JetBrainsMono Nerd Font:h12" end
 
--- replace alpha footer 'AstroNvim' with 'VSCode'
--- local utils = require "utils"
--- local cfg_path = vim.fn.stdpath "config"
--- if utils.is_win then os.execute(tostring("sh " .. cfg_path .. "/scripts/win/alpha_replace.sh")) end
--- if utils.is_mac then os.execute(tostring("sh " .. cfg_path .. "/scripts/darwin/alpha_replace.sh")) end
--- if utils.is_linux then os.execute(tostring("sh " .. cfg_path .. "/scripts/alpha_replace.sh")) end
-
--- renaming rust dap
+-- renaming rust dap for it to be recognised
 require("dap").adapters.lldb = require("dap").adapters.codelldb
 
 -- require("lspconfig").taplo.setup {}
