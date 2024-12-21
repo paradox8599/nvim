62f1bbeaa48c6608ec77f10da0ed4eb9be478e07
paradox8599 <paradox8599@outlook.com>
2024-12-22 04:38:45 +1100

    disabled emmet_ls

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,16 +3,16 @@
 ---@type LazySpec
 return {
   -- use mason-lspconfig to configure LSP installations
-  -- {
-  --   "williamboman/mason-lspconfig.nvim",
-  --   -- overrides `require("mason-lspconfig").setup(...)`
-  --   opts = {
-  --     ensure_installed = {
-  --       "lua_ls",
-  --       -- add more arguments for adding more language servers
-  --     },
-  --   },
-  -- },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    -- overrides `require("mason-lspconfig").setup(...)`
+
+    -- https://www.reddit.com/r/AstroNvim/comments/1hcktn5/disable_emmet_from_packtypescript/
+    opts = function(_, opts)
+      opts.ensure_installed = vim.tbl_filter(function(s) return s ~= "emmet_ls" end, opts.ensure_installed)
+    end,
+  },
+
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
     "jay-babu/mason-null-ls.nvim",
@@ -25,6 +25,7 @@ return {
       },
     },
   },
+
   {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
