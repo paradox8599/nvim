592c316db095548d01183f058917779fd0d4473a
paradox8599 <paradox8599@outlook.com>
2025-01-07 14:25:52 +1100

    global snippets

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -18,15 +18,20 @@ return {
   -- },
 
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  -- {
-  --   "L3MON4D3/LuaSnip",
-  --   config = function(plugin, opts)
-  --     require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-  --     -- add more custom luasnip configuration such as filetype extend or custom snippets
-  --     local luasnip = require "luasnip"
-  --     luasnip.filetype_extend("javascript", { "javascriptreact" })
-  --   end,
-  -- },
+  {
+    "L3MON4D3/LuaSnip",
+    config = function(plugin, opts)
+      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+      -- add more custom luasnip configuration such as filetype extend or custom snippets
+      local luasnip = require "luasnip"
+      luasnip.filetype_extend("javascript", { "javascriptreact" })
+      luasnip.filetype_extend("typescript", { "javascript", "javascriptreact", "typescriptreact" })
+      -- load snippets paths
+      require("luasnip.loaders.from_vscode").lazy_load {
+        paths = { vim.fn.stdpath "config" .. "/snippets" },
+      }
+    end,
+  },
 
   {
     "nvim-neo-tree/neo-tree.nvim",
