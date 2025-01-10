b00d0c971b0ce021f2a8569f777510db71cd384b
paradox8599 <paradox8599@outlook.com>
2025-01-11 02:47:13 +1100

    live-rename

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -157,6 +157,28 @@ return {
     opts = {},
   },
 
+  {
+    "saecki/live-rename.nvim",
+    dependencies = {
+      {
+        "AstroNvim/astrolsp",
+        optional = true,
+        ---@type AstroLSPOpts
+        opts = {
+          mappings = {
+            n = {
+              ["<Leader>lr"] = {
+                function() require("live-rename").rename { insert = true } end,
+                desc = "Rename current symbol",
+              },
+            },
+          },
+        },
+      },
+    },
+    opts = {},
+  },
+
   -- sad
   -- {
   --   "ray-x/sad.nvim",
