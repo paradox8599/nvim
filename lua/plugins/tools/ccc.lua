42a8cdbbafaf088c39e3ee91c4cff76bd2d5f0b1
paradox8599 <paradox8599@outlook.com>
2025-01-12 07:14:01 +1100

    lazy configs; transparent

---
+++ lua/plugins/tools/ccc.lua
@@ -0,0 +1,20 @@
+-- color picker & visualizer
+
+return {
+  "uga-rosa/ccc.nvim",
+
+  event = { "User AstroFile", "InsertEnter" },
+
+  keys = {
+    { "<Leader>uC", vim.cmd.CccHighlighterToggle, desc = "Toggle colorizer" },
+    { "<Leader>zc", vim.cmd.CccConvert, desc = "Convert color" },
+    { "<Leader>zp", vim.cmd.CccPick, desc = "Pick color" },
+  },
+
+  specs = { { "NvChad/nvim-colorizer.lua", enabled = false } },
+
+  config = function(_, opts)
+    require("ccc").setup(opts)
+    vim.cmd.CccHighlighterEnable()
+  end,
+}
