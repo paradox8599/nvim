4496db906a88e09dc6f9aeb35859fa80ebd01876
paradox8599 <paradox8599@outlook.com>
2025-01-12 02:55:08 +1100

    precognition

---
+++ lua/plugins/tools/precognition.lua
@@ -0,0 +1,37 @@
+-- show a preview of the motion keys for navigating the current buffer
+
+return {
+  "tris203/precognition.nvim",
+
+  keys = {
+    { "<leader>uP", function() require("precognition").toggle() end, desc = "Toggle [P]recognition" },
+    { "<leader>P", function() require("precognition").peek() end, desc = "[P]eek Precognition" },
+  },
+
+  opts = {
+    startVisible = false,
+    showBlankVirtLine = false,
+    -- highlightColor = { link = "Comment" },
+    -- hints = {
+    --      Caret = { text = "^", prio = 2 },
+    --      Dollar = { text = "$", prio = 1 },
+    --      MatchingPair = { text = "%", prio = 5 },
+    --      Zero = { text = "0", prio = 1 },
+    --      w = { text = "w", prio = 10 },
+    --      b = { text = "b", prio = 9 },
+    --      e = { text = "e", prio = 8 },
+    --      W = { text = "W", prio = 7 },
+    --      B = { text = "B", prio = 6 },
+    --      E = { text = "E", prio = 5 },
+    -- },
+    -- gutterHints = {
+    --     G = { text = "G", prio = 10 },
+    --     gg = { text = "gg", prio = 9 },
+    --     PrevParagraph = { text = "{", prio = 8 },
+    --     NextParagraph = { text = "}", prio = 8 },
+    -- },
+    -- disabled_fts = {
+    --     "startify",
+    -- },
+  },
+}
