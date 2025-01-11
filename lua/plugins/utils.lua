bf5fd45147cf09917df30a4483ff43b529a40f11
paradox8599 <paradox8599@outlook.com>
2025-01-12 00:28:10 +1100

    removed unused

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -69,45 +69,4 @@ return {
       { "<leader>um", function() require("render-markdown").toggle() end, desc = "Toggle Render Markdown" },
     },
   },
-
-  -- harpoon
-  -- {
-  --   "ThePrimeagen/harpoon",
-  --   branch = "harpoon2",
-  --   requires = { { "nvim-lua/plenary.nvim" } },
-  --   event = "VeryLazy",
-  --   opts = {},
-  --   keys = {
-  --     {
-  --       "<leader><leader>a",
-  --       function() require("harpoon"):list():add() end,
-  --       desc = "[Marks] Add file to marks",
-  --     },
-  --     {
-  --       "<leader><leader>d",
-  --       function() require("harpoon"):list():remove() end,
-  --       desc = "[Marks] Remove file from marks",
-  --     },
-  --     {
-  --       "<leader><leader>c",
-  --       function() require("harpoon"):list():clear() end,
-  --       desc = "[Marks] Clear marks",
-  --     },
-  --     {
-  --       "<leader><leader>m",
-  --       function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end,
-  --       desc = "[Marks] Toggle harpoon marks menu",
-  --     },
-  --     {
-  --       "<C-p>",
-  --       function() require("harpoon"):list():prev() end,
-  --       desc = "[Marks] Goto previous mark",
-  --     },
-  --     {
-  --       "<C-n>",
-  --       function() require("harpoon"):list():next() end,
-  --       desc = "[Marks] Goto next mark",
-  --     },
-  --   },
-  -- },
 }
