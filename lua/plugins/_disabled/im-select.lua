47afeb6c562114d645b88b98d364a6eb13596965
paradox8599 <paradox8599@outlook.com>
2025-01-12 16:25:10 +1100

    disabled im-select

--- lua/plugins/_disabled/im-select.lua
+++ lua/plugins/_disabled/im-select.lua
@@ -2,7 +2,7 @@ local os = require "lib.os_detect"
 
 return {
   "keaising/im-select.nvim",
-  enabled = false and not os.is_linux,
+  enabled = not os.is_linux,
   event = "VeryLazy",
   opts = {
     default_im_select = os.is_win and "3081" or os.is_mac and "com.apple.keylayout.Australian" or "us",
