f1e4477ba8f35e0ef4e043daaa85bdcbfcdcfee2
paradox8599 <paradox8599@outlook.com>
2025-01-18 04:57:59 +1100

    smartcolumn config

---
+++ lua/plugins/ui/smartcolumn.lua
@@ -0,0 +1,18 @@
+return {
+  "m4xshen/smartcolumn.nvim",
+
+  event = { "InsertEnter", "User AstroFile" },
+
+  opts = {
+    disabled_filetypes = {
+      "alpha",
+      "neo-tree",
+      "ministarter",
+      "help",
+      "text",
+      "markdown",
+      "oil",
+      "octo",
+    },
+  },
+}
