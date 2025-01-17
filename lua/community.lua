67e5db426e8fe4c70f453e15bb875f1100129c55
paradox8599 <paradox8599@outlook.com>
2025-01-18 04:56:10 +1100

    transparent config

--- lua/community.lua
+++ lua/community.lua
@@ -5,8 +5,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity/bars-and-lines/smartcolumn-nvim" }, -- line max characters indicator
 
-  { import = "astrocommunity/color/transparent-nvim" },
-
   { import = "astrocommunity/completion/cmp-cmdline" }, -- command auto completion
   -- { import = "astrocommunity/diagnostics/trouble-nvim" }, -- diagnostic UI
   -- { import = "astrocommunity/editing-support/nvim-regexplainer" }, -- explain regex
