85fc2b856ffaa3de88dc74dd494125f4cf32ec71
paradox8599 <paradox8599@outlook.com>
2025-01-19 00:58:24 +1100

    disabled firenvim

--- lua/plugins/_disabled/firenvim.lua
+++ lua/plugins/_disabled/firenvim.lua
@@ -7,16 +7,30 @@ return {
   build = ":call firenvim#install(0)",
   config = function()
     vim.g.firenvim_config = {
-      globalSettings = { alt = "all" },
-      localSettings = { [".*"] = { content = "text", takeover = "never" } },
+      globalSettings = {
+        alt = "all",
+      },
+      localSettings = {
+        [".*"] = {
+          cmdline = "neovim",
+          content = "text",
+          takeover = "never",
+          -- priority = 0,
+          -- selector = "textarea",
+        },
+      },
     }
+
     vim.api.nvim_create_autocmd({ "UIEnter" }, {
       callback = function()
-        if vim.o.lines < 16 then vim.o.lines = 16 end
-        if vim.o.columns < 80 then vim.o.columns = 80 end
-        vim.cmd [[set background=light]]
+        if vim.g.started_by_firenvim then
+          if vim.o.lines < 16 then vim.o.lines = 16 end
+          if vim.o.columns < 80 then vim.o.columns = 80 end
+          vim.cmd [[set background=light]]
+        end
       end,
     })
+
     -- autosave
     -- vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
     --   nested = true,
