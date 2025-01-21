8e58277c19f4e80bc18351ce7d05abd5a28b9706
paradox8599 <paradox8599@outlook.com>
2025-01-21 21:44:39 +1100

    disabled tiny-inline-diagnostic

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,7 +1,3 @@
--- This will run last in the setup process and is a good place to configure
--- things like custom filetypes. This just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
-
 -- set font for neovide
 if vim.g.neovide then vim.o.guifont = "JetBrainsMono Nerd Font:h12" end
 
@@ -9,3 +5,5 @@ if vim.g.neovide then vim.o.guifont = "JetBrainsMono Nerd Font:h12" end
 require("dap").adapters.lldb = require("dap").adapters.codelldb
 
 -- require("lspconfig").taplo.setup {}
+
+vim.diagnostic.config { virtual_text = false }
