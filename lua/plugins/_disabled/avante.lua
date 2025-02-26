3466c17b8beb8f103f30ae22abf07af0d949252f
paradox8599 <paradox8599@outlook.com>
2025-02-27 05:42:06 +1100

    avante configs & updated

--- lua/plugins/_disabled/avante.lua
+++ lua/plugins/_disabled/avante.lua
@@ -4,8 +4,8 @@ return {
   version = "*", -- set this to "*" if you want to always pull the latest change, false to update on release
 
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  -- build = "make",
+  build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false", -- for windows
   dependencies = {
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
@@ -13,24 +13,7 @@ return {
     --- The below dependencies are optional,
     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
-        },
-      },
-    },
+    -- "zbirenbaum/copilot.lua", -- for providers='copilot'
     {
       -- Make sure to set this up properly if you have lazy=true
       "MeanderingProgrammer/render-markdown.nvim",
@@ -42,30 +25,22 @@ return {
   },
 
   opts = {
-    provider = "ollama",
-    vendors = {
-      ollama = {
-        -- endpoint = "http://172.29.80.1:11434/api/generate",
-        model = "phi4",
-        -- parse_curl_args = function(opts, code_opts)
-        --   return {
-        --     url = opts.endpoint,
-        --     headers = {
-        --       ["Accept"] = "application/json",
-        --       ["Content-Type"] = "application/json",
-        --     },
-        --     body = {
-        --       model = opts.model,
-        --       prompt = require("avante.providers").ollama.parse_message(code_opts),
-        --       -- max_tokens = 4096,
-        --       stream = true,
-        --     },
-        --   }
-        -- end,
-        -- parse_response_data = function(data_stream, event_state, opts)
-        --   require("avante.providers").openai.parse_response(data_stream, event_state, opts)
-        -- end,
-      },
+    provider = "openai",
+    auto_suggestions_provider = "openai",
+    openai = {
+      endpoint = "",
+      model = "",
+      temperature = 0.6,
+      max_tokens = 8192,
+      reasoning_effort = "medium",
     },
+    windows = { width = 45 },
+    behaviour = {
+      auto_suggestions = false,
+    },
+  },
+
+  keys = {
+    { "<leader>an", "<cmd>AvanteClear<cr>", desc = "New Chat" },
   },
 }
