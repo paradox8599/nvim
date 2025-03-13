34fa207b9ff492ba9f6649c37303db313d0f37ef
paradox8599 <paradox8599@outlook.com>
2025-03-13 23:04:41 +1100

    added nerdy.nvim

---
+++ lua/plugins/tools/nerdy.lua
@@ -0,0 +1,10 @@
+return {
+  "2kabhishek/nerdy.nvim",
+  dependencies = {
+    "folke/snacks.nvim",
+  },
+  cmd = "Nerdy",
+  keys = {
+    { "<leader>fe", "<cmd>Nerdy<cr>", desc = "Nerdy" },
+  },
+}
