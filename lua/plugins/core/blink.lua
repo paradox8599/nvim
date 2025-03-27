0c9fee23065fac20e51a4ab88992082c6a36ee87
paradox8599 <paradox8599@outlook.com>
2025-03-27 17:39:23 +1100

    blink.nvim config

--- lua/plugins/core/blink.lua
+++ lua/plugins/core/blink.lua
@@ -1,5 +1,76 @@
+-- docs: https://cmp.saghen.dev
+
 return {
   {
     "Saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "none",
+        ["<CR>"] = {},
+        ["<Enter>"] = {},
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-x>"] = { "show_documentation", "hide_documentation" },
+        ["<C-e>"] = { "hide" },
+        ["<C-y>"] = { "select_and_accept" },
+
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
+        ["<C-p>"] = { "show", "select_prev", "fallback_to_mappings" },
+        ["<C-n>"] = { "show", "select_next", "fallback_to_mappings" },
+
+        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+        ["<Tab>"] = { "snippet_forward", "fallback" },
+        ["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+        ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+        ["<C-j>"] = { "fallback_to_mappings" },
+      },
+
+      cmdline = {
+        keymap = {
+          ["<Tab>"] = { "show", "accept" },
+          ["<CR>"] = { "accept_and_enter", "fallback" },
+        },
+        completion = {
+          ghost_text = { enabled = true },
+          menu = { auto_show = true },
+        },
+      },
+
+      completion = {
+        -- 'prefix' will fuzzy match on the text before the cursor
+        -- 'full' will fuzzy match on the text before _and_ after the cursor
+        -- example: 'foo_|_bar' will match 'foo_' for 'prefix' and 'foo__bar' for 'full'
+        keyword = { range = "full" },
+
+        accept = { auto_brackets = { enabled = true } },
+
+        documentation = { auto_show = true, auto_show_delay_ms = 150 },
+
+        list = { selection = { preselect = false, auto_insert = false } },
+
+        menu = { auto_show = true },
+
+        ghost_text = { enabled = true },
+      },
+
+      fuzzy = { implementation = "prefer_rust_with_warning" },
+
+      sources = {
+        -- Remove 'buffer' if you don't want text completions, by default it's only enabled when LSP returns no items
+        -- default = { "lsp", "path", "luasnip", "snippets", "buffer", "omni" },
+      },
+
+      -- Use a preset for snippets, check the snippets documentation for more information
+      -- 'default' | 'luasnip' | 'mini_snippets'
+      snippets = { preset = "luasnip" },
+
+      signature = {
+        enabled = true,
+        window = { show_documentation = true },
+      },
+    },
   },
 }
