8ce0234b19392682b67145e846ebb69040da8f5a
paradox8599 <paradox8599@outlook.com>
2025-03-31 11:35:34 +1100

    rm unused config

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,2 @@
--- renaming rust dap for it to be recognised
-require("dap").adapters.lldb = require("dap").adapters.codelldb
-
 -- turn off virtual text for tiny inline diagnostic
 vim.diagnostic.config { virtual_text = false }
