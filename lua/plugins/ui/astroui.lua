5477acf452b8951979d7e1526b8e31df83944635
paradox8599 <paradox8599@outlook.com>
2025-04-07 13:11:32 +1000

    feat: added plugin posterpole.nvim disabled

--- lua/plugins/ui/astroui.lua
+++ lua/plugins/ui/astroui.lua
@@ -1,39 +1,67 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- AstroUI provides the basis for configuring the AstroNvim User Interface
--- Configuration documentation can be found with `:h astroui`
--- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
---       as this provides autocomplete and documentation while editing
-
----@type LazySpec
 return {
-  "AstroNvim/astroui",
-  ---@type AstroUIOpts
-  opts = {
-    -- change colorscheme
-    colorscheme = "astrodark",
-    -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
-    highlights = {
-      init = { -- this table overrides highlights in all themes
-        -- Normal = { bg = "#000000" },
+  ---@type LazySpec
+  {
+    "AstroNvim/astroui",
+    ---@type AstroUIOpts
+    opts = {
+      -- change colorscheme
+      colorscheme = "astrodark",
+      -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
+      highlights = {
+        init = { -- this table overrides highlights in all themes
+          -- Normal = { bg = "#000000" },
+        },
+        astrodark = { -- a table of overrides/changes when applying the astrotheme theme
+          -- Normal = { bg = "#000000" },
+        },
       },
-      astrodark = { -- a table of overrides/changes when applying the astrotheme theme
-        -- Normal = { bg = "#000000" },
+      -- Icons can be configured throughout the interface
+      icons = {
+        -- configure the loading of the lsp in the status line
+        LSPLoading1 = "⠋",
+        LSPLoading2 = "⠙",
+        LSPLoading3 = "⠹",
+        LSPLoading4 = "⠸",
+        LSPLoading5 = "⠼",
+        LSPLoading6 = "⠴",
+        LSPLoading7 = "⠦",
+        LSPLoading8 = "⠧",
+        LSPLoading9 = "⠇",
+        LSPLoading10 = "⠏",
       },
-    },
-    -- Icons can be configured throughout the interface
-    icons = {
-      -- configure the loading of the lsp in the status line
-      LSPLoading1 = "⠋",
-      LSPLoading2 = "⠙",
-      LSPLoading3 = "⠹",
-      LSPLoading4 = "⠸",
-      LSPLoading5 = "⠼",
-      LSPLoading6 = "⠴",
-      LSPLoading7 = "⠦",
-      LSPLoading8 = "⠧",
-      LSPLoading9 = "⠇",
-      LSPLoading10 = "⠏",
     },
   },
+  -- {
+  --   "ilof2/posterpole.nvim",
+  --   priority = 1000,
+  --   config = function()
+  --     local posterpole = require "posterpole"
+  --
+  --     posterpole.setup {
+  --       transparent = false,
+  --       colorless_bg = false,
+  --       dim_inactive = false,
+  --       brightness = 1,
+  --       selected_tab_highlight = false,
+  --       fg_saturation = 0,
+  --       bg_saturation = 0,
+  --
+  --       adaptive_brightness = {
+  --         enabled = true,
+  --         max_brightness = 3,
+  --         min_brightness = -6,
+  --         daylight_duration = 12,
+  --         noon_time = 12,
+  --       },
+  --
+  --       lualine = { transparent = true },
+  --     }
+  --
+  --     vim.cmd "colorscheme posterpole"
+  --
+  --     -- This function create sheduled task, which will reload theme every hour
+  --     -- Without "setup_adaptive" adaptive brightness will be set only after every restart
+  --     posterpole.setup_adaptive()
+  --   end,
+  -- },
 }
